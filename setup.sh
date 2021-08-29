0e6f44bf0c8c58ce6109b5309ff8337789341c4b
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-08-29 08:54:20 -0700

    updated

--- setup.sh
+++ setup.sh
@@ -3,27 +3,27 @@
 compose=$PWD/.global/docker-compose.yml
 path=${dir}/public_html
 
-mkdir -p ${path}
+noroot mkdir -p ${path}
 
 
 if [[ "none" == ${type} ]]; then
     if [[ ! -f "${path}/index.php" ]]; then
-        touch "${path}/index.php"
+        noroot touch "${path}/index.php"
     fi
 elif [[ "laravel" == ${type} ]]; then
     if [[ ! -f "${path}/composer.json" ]]; then
-        composer create-project laravel/laravel ${path}
+        noroot composer create-project laravel/laravel ${path}
     fi
 elif [[ "ClassicPress" == ${type} ]]; then
     if [[ ! -f "${path}/wp-config.php" ]]; then
-        wp core download --quiet --path="${path}" https://github.com/ClassicPress/ClassicPress-release/archive/1.3.0-rc2.zip
-        wp config create --dbhost=localhost --dbname=${domain} --dbuser=classicpress --dbpass=classicpress --quiet --path="${path}"
-        wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
+        noroot wp core download --quiet --path="${path}" https://github.com/ClassicPress/ClassicPress-release/archive/1.3.0-rc2.zip
+        noroot wp config create --dbhost=localhost --dbname=${domain} --dbuser=classicpress --dbpass=classicpress --quiet --path="${path}"
+        noroot wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
         
         if [[ "${plugins}" != "none" ]]; then
           for plugin in ${plugins//- /$'\n'}; do
             if [[ "${plugin}" != "plugins" ]]; then
-              wp plugin install ${plugin} --activate --quiet --path="${path}"
+              noroot wp plugin install ${plugin} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -31,7 +31,7 @@ elif [[ "ClassicPress" == ${type} ]]; then
         if [[ "${themes}" != "none" ]]; then
           for theme in ${themes//- /$'\n'}; do
             if [[ "${theme}" != "themes" ]]; then
-              wp theme install ${theme} --activate --quiet --path="${path}"
+              noroot wp theme install ${theme} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -39,29 +39,29 @@ elif [[ "ClassicPress" == ${type} ]]; then
         if [[ "${constants}" != "none" ]]; then
           for const in ${constants//- /$'\n'}; do
             if [[ "${const}" != "constants" ]]; then
-              wp config set --type=constant ${const} --raw true --quiet --path="${path}"
+              noroot wp config set --type=constant ${const} --raw true --quiet --path="${path}"
             fi
           done
         fi
     fi
 else
     if [[ ! -f "${path}/wp-config.php" ]]; then
-        wp core download --quiet --path="${path}"
-        wp config create --dbhost=localhost --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --quiet --path="${path}"
-        wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
+        noroot wp core download --quiet --path="${path}"
+        noroot wp config create --dbhost=localhost --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --quiet --path="${path}"
+        noroot wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
 
         if [[ -d "${path}/wp-content/plugins/akismet" ]]; then
-          wp plugin delete akismet --quiet --path="${path}"
+          noroot wp plugin delete akismet --quiet --path="${path}"
         fi 
 
         if [[ -f "${path}/wp-content/plugins/hello.php" ]]; then
-          wp plugin delete hello --quiet --path="${path}"
+          noroot wp plugin delete hello --quiet --path="${path}"
         fi 
 
         if [[ "${plugins}" != "none" ]]; then
           for plugin in ${plugins//- /$'\n'}; do
             if [[ "${plugin}" != "plugins" ]]; then
-              wp plugin install ${plugin} --activate --quiet --path="${path}"
+              noroot wp plugin install ${plugin} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -69,7 +69,7 @@ else
         if [[ "${themes}" != "none" ]]; then
           for theme in ${themes//- /$'\n'}; do
             if [[ "${theme}" != "themes" ]]; then
-              wp theme install ${theme} --activate --quiet --path="${path}"
+              noroot wp theme install ${theme} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -77,10 +77,9 @@ else
         if [[ "${constants}" != "none" ]]; then
           for const in ${constants//- /$'\n'}; do
             if [[ "${const}" != "constants" ]]; then
-              wp config set --type=constant ${const} --raw true --quiet --path="${path}"
+              noroot wp config set --type=constant ${const} --raw true --quiet --path="${path}"
             fi
           done
         fi
     fi
 fi
-
