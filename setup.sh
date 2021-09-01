a52645c2b06399d375bb3d97ad368110c576342f
benlumia007 <benlumia007@users.noreply.github.com>
2021-09-01 08:28:51 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ elif [[ "laravel" == ${type} ]]; then
     fi
 elif [[ "ClassicPress" == ${type} ]]; then
     if [[ ! -f "${path}/wp-config.php" ]]; then
-        noroot wp core download --quiet --path="${path}" https://github.com/ClassicPress/ClassicPress-release/archive/1.3.0-rc2.zip
+        noroot wp core download --quiet --path="${path}" https://classicpress.net/latest.zip
         noroot wp config create --dbhost=localhost --dbname=${domain} --dbuser=classicpress --dbpass=classicpress --quiet --path="${path}"
         noroot wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
         
