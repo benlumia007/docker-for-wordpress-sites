12d5b6624b77a4436f1b0d4a6c1948a20ead269b
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-09-12 22:14:49 -0700

    updated

--- setup.sh
+++ setup.sh
@@ -46,17 +46,13 @@ elif [[ "ClassicPress" == ${type} ]]; then
     fi
 else
     if [[ ! -f "${path}/wp-config.php" ]]; then
-      if [[ ${ms} == 'sub-domain' ]]; then 
-        wp core download --quiet --path="${path}"
-        wp config create --dbhost=localhost --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --quiet --path="${path}"
-        wp core multisite-install --subdomains --url="${domain}" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --path="${path}"
-
-      elif [[ ${ms} == 'sub-directory' ]]; then
-        echo ${ms}
-      else
         wp core download --quiet --path="${path}"
         wp config create --dbhost=localhost --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --quiet --path="${path}"
         wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
+
+        if [[ ${ms} == 'sub-domain' ]]; then
+              noroot wp core multisite-install --subdomains --url="${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
+        fi
 
         if [[ -d "${path}/wp-content/plugins/akismet" ]]; then
           wp plugin delete akismet --quiet --path="${path}"
@@ -89,6 +85,5 @@ else
             fi
           done
         fi
-      fi
     fi
 fi
