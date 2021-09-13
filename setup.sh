ddcd9802f0723e9a225938875873ab966d0e7756
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-09-13 07:49:45 -0700

    updated

--- setup.sh
+++ setup.sh
@@ -16,14 +16,14 @@ elif [[ "laravel" == ${type} ]]; then
     fi
 elif [[ "ClassicPress" == ${type} ]]; then
     if [[ ! -f "${path}/wp-config.php" ]]; then
-        wp core download  --path="${path}" https://classicpress.net/latest.zip
-        wp config create --dbhost=localhost --dbname=${domain} --dbuser=classicpress --dbpass=classicpress  --path="${path}"
-        wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email  --path="${path}"
+        wp core download --quiet --path="${path}" https://classicpress.net/latest.zip
+        wp config create --dbhost=localhost --dbname=${domain} --dbuser=classicpress --dbpass=classicpress --quiet --path="${path}"
+        wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
         
         if [[ "${plugins}" != "none" ]]; then
           for plugin in ${plugins//- /$'\n'}; do
             if [[ "${plugin}" != "plugins" ]]; then
-              wp plugin install ${plugin} --activate  --path="${path}"
+              wp plugin install ${plugin} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -31,7 +31,7 @@ elif [[ "ClassicPress" == ${type} ]]; then
         if [[ "${themes}" != "none" ]]; then
           for theme in ${themes//- /$'\n'}; do
             if [[ "${theme}" != "themes" ]]; then
-              wp theme install ${theme} --activate  --path="${path}"
+              wp theme install ${theme} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -39,37 +39,37 @@ elif [[ "ClassicPress" == ${type} ]]; then
         if [[ "${constants}" != "none" ]]; then
           for const in ${constants//- /$'\n'}; do
             if [[ "${const}" != "constants" ]]; then
-              wp config set --type=constant ${const} --raw true  --path="${path}"
+              wp config set --type=constant ${const} --raw true --quiet --path="${path}"
             fi
           done
         fi
     fi
 else
     if [[ ! -f "${path}/wp-config.php" ]]; then
-        wp core download  --path="${path}"
-        wp config create --dbhost=localhost --dbname=${domain} --dbuser=wordpress --dbpass=wordpress  --path="${path}"
-        wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email  --path="${path}"
+        wp core download --quiet --path="${path}"
+        wp config create --dbhost=localhost --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --quiet --path="${path}"
+        wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
 
         if [[ "${ms}" == 'sub-domain' ]]; then
-          wp core multisite-install --subdomains --url="${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email  --path="${path}"
+          wp core multisite-install --subdomains --url="${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
         elif [[ "${ms}" == 'sub-directory' ]]; then
           echo "installing ${ms}"
-          wp core multisite-install --url="${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email  --path="${path}"
+          wp core multisite-install --url="${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path="${path}"
 
         fi
 
         if [[ -d "${path}/wp-content/plugins/akismet" ]]; then
-          wp plugin delete akismet  --path="${path}"
+          wp plugin delete akismet --quiet --path="${path}"
         fi 
 
         if [[ -f "${path}/wp-content/plugins/hello.php" ]]; then
-          wp plugin delete hello  --path="${path}"
+          wp plugin delete hello --quiet --path="${path}"
         fi 
 
         if [[ "${plugins}" != "none" ]]; then
           for plugin in ${plugins//- /$'\n'}; do
             if [[ "${plugin}" != "plugins" ]]; then
-              wp plugin install ${plugin} --activate  --path="${path}"
+              wp plugin install ${plugin} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -77,7 +77,7 @@ else
         if [[ "${themes}" != "none" ]]; then
           for theme in ${themes//- /$'\n'}; do
             if [[ "${theme}" != "themes" ]]; then
-              wp theme install ${theme} --activate  --path="${path}"
+              wp theme install ${theme} --activate --quiet --path="${path}"
             fi
           done
         fi
@@ -85,7 +85,7 @@ else
         if [[ "${constants}" != "none" ]]; then
           for const in ${constants//- /$'\n'}; do
             if [[ "${const}" != "constants" ]]; then
-              wp config set --type=constant ${const} --raw true  --path="${path}"
+              wp config set --type=constant ${const} --raw true --quiet --path="${path}"
             fi
           done
         fi
